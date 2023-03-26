5db8a5a53b4cb272b6dbab0aa5bdb751d6088760
Luis Guilherme <luislebert01@outlook.com>
2023-03-26 16:12:15 -0300

    Lógica inicial da program

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using TodoList.Lists;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+            Console.WriteLine("Menu de opções");
+            Console.WriteLine("1 - Criar tarefa");
+            Console.WriteLine("2 - Visualizar tarefas");
+    }
+
+    private static bool CreateTask()
+    {
+        Console.WriteLine("Nome da tarefa: ");
+        string description = Console.ReadLine();
+
+        Console.WriteLine("Dono da tarefa: ");
+        string ownerName = Console.ReadLine();
+
+        Person owner = new Person(ownerName);
+
+        Todo todo = new Todo(description, owner);
+        return true;
     }
 }

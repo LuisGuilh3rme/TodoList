d3af9b9bf5d040374f82fa9713467e01f171168b
André misuraca <m1sur4k@gmail.com>
2023-03-26 18:59:46 -0300

    modificado parte de visualizar e modificar

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -2,20 +2,12 @@
 using TodoList.Lists;
 internal class Program
 {
+    public static List<Person> people = new List<Person>();
+    public static List<string> categories = new List<string>();
+    public static List<Todo> todos = new List<Todo>();
     private static void Main(string[] args)
     {
         Menu();
-    }
-    private static bool CreateTask()
-    {
-        Console.Clear();
-        Console.Write("Nome da tarefa: ");
-        string description = Console.ReadLine();
-        Console.Write("Dono da tarefa: ");
-        string ownerName = Console.ReadLine();
-        Person owner = new Person(ownerName);
-        Todo todo = new Todo(description, owner);
-        return true;
     }
     static void Menu()
     {
@@ -34,13 +26,13 @@ internal class Program
             if (!valid)
             {
                 Console.WriteLine("Opção inválida! Tente novamente.");
-                Thread.Sleep(1000);
-                Console.Clear();
+                Console.WriteLine("Digite ENTER para continuar");
+                Console.ReadLine();
             }
-            else Choice(escolha);
+            else valid = Choice(escolha);
         }
     }
-    static void Choice(int opt)
+    static bool Choice(int opt)
     {
         switch (opt)
         {
@@ -49,22 +41,96 @@ internal class Program
                 break;
             case 2:
                 // chamar metodo para visualizar tarefas
+                PrintList();
+                Console.ReadLine();
                 break;
             case 3:
                 // chamar metodo para remover tarefas
                 break;
             case 4:
                 // chamar metodo para editar tarefas
+                EditTask();
                 break;
             case 0:
                 Console.WriteLine("Saindo do programa...");
                 break;
             default:
                 Console.WriteLine("Opção inválida! Tente novamente.");
-                Thread.Sleep(1000);
-                Console.Clear();
-                Menu();
+                Console.WriteLine("Digite ENTER para continuar");
+                Console.ReadLine();
+                return false;
+        }
+        return true;
+    }
+    private static bool CreateTask()
+    {
+        Console.Clear();
+        Console.Write("Nome da tarefa: ");
+        string description = Console.ReadLine();
+        Console.Write("Dono da tarefa: ");
+        string ownerName = Console.ReadLine();
+        Person owner = new Person(ownerName);
+        todos.Add(new Todo(description, owner));
+        return true;
+    }
+    private static bool EditTask()
+    {
+        Console.Clear();
+        Console.WriteLine("== EDIT LIST ==");
+        PrintList();
+        Console.WriteLine("Escolha um item da lista para edição: ");
+        int.TryParse(Console.ReadLine(), out int index);
+        if (todos.Count < index || index < 1)
+        {
+            Console.WriteLine("Index inválido");
+            Console.WriteLine("Digite ENTER para continuar");
+            Console.ReadLine();
+            return false;
+        }
+        index = index - 1;
+        int editOption = 0;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine(todos[index].ToString());
+            Console.WriteLine("O que deseja editar?");
+            Console.WriteLine("1 - Editar pessoa");
+            Console.WriteLine("2 - Editar descrição");
+            Console.WriteLine("3 - Editar categoria");
+            Console.WriteLine("4 - Editar data final");
+            int.TryParse(Console.ReadLine(), out editOption);
+            if (editOption > 4 || editOption < 1) editOption = 0;
+        } while (editOption == 0);
+        switch (editOption)
+        {
+            case 1:
+                todos[index].setPerson(new Person("JUBILEU"));
                 break;
+            case 2:
+                Console.WriteLine("Insira a nova descrição: ");
+                string description = Console.ReadLine();
+                todos[index].setDescription(description);
+                break;
+            case 3:
+                Console.WriteLine("Insira a nova categoria: ");
+                string category = Console.ReadLine();
+                todos[index].setCategory(category);
+                break;
+            case 4:
+                Console.WriteLine("Insira a data de finalização (mm/dd/yyyy): ");
+                string date = Console.ReadLine();
+                todos[index].setDueDate(date);
+                break;
+        }
+        Console.ReadLine();
+        return true;
+    }
+    private static void PrintList()
+    {
+        int count = 0;
+        foreach (Todo todo in todos)
+        {
+            Console.WriteLine($"{++count}) " + todo.ToString());
         }
     }
 }
